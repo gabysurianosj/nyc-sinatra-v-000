bb9e4516c9c20befebc3d005e5ede348f7238c79
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-15 15:24:54 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < Sinatra::Base
   set :views, proc { File.join(root, '../views/') }
   register Sinatra::Twitter::Bootstrap::Assets
-  set :session_secret, "my_application_secret"
-  set :views, Proc.new { File.join(root, "../views/") }
+  
 
   get '/' do
     erb :"application/index"
